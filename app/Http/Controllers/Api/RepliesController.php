650ce5438c7a6ccc6cb96eb20379834bb75dd8ce
annawoo <chinaanna@outlook.com>
2018-12-11 15:19:57 +0800

    删除回复

--- app/Http/Controllers/Api/RepliesController.php
+++ app/Http/Controllers/Api/RepliesController.php
@@ -20,4 +20,15 @@ class RepliesController extends Controller
             ->setStatusCode(201);
     }
 
+    public function destroy(Topic $topic, Reply $reply)
+    {
+        if ($reply->topic_id != $topic->id) {
+            return $this->response->errorBadRequest();
+        }
+
+        $this->authorize('destroy', $reply);
+        $reply->delete();
+
+        return $this->response->noContent();
+    }
 }
