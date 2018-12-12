786e377e9243d6b5d27dfabcc93181dc236566cf
annawoo <chinaanna@outlook.com>
2018-12-12 17:48:07 +0800

    jwt认证改为passport认证

--- app/Http/Controllers/Api/AuthorizationsController.php
+++ app/Http/Controllers/Api/AuthorizationsController.php
@@ -7,24 +7,37 @@ use App\Http\Requests\Api\AuthorizationRequest;
 use App\Http\Requests\Api\SocialAuthorizationRequest;
 use App\Models\User;
 use Illuminate\Http\Request;
+use League\OAuth2\Server\AuthorizationServer;
+use League\OAuth2\Server\Exception\OAuthServerException;
+use Psr\Http\Message\ServerRequestInterface;
+use Zend\Diactoros\Response as Psr7Response;
 
 class AuthorizationsController extends Controller
 {
-    public function store(AuthorizationRequest $request)
+//    public function store(AuthorizationRequest $request)
+//    {
+//        $username = $request->username;
+//
+//        filter_var($username, FILTER_VALIDATE_EMAIL) ?
+//            $credentials['email'] = $username :
+//            $credentials['phone'] = $username;
+//
+//        $credentials['password'] = $request->password;
+//
+//        if (!$token = Auth::guard('api')->attempt($credentials)) {
+//            return $this->response->errorUnauthorized('用户名或密码错误');
+//        }
+//
+//        return $this->respondWithToken($token)->setStatusCode(201);
+//    }
+
+    public function store(AuthorizationRequest $originRequest, AuthorizationServer $server, ServerRequestInterface $serverRequest)
     {
-        $username = $request->username;
-
-        filter_var($username, FILTER_VALIDATE_EMAIL) ?
-            $credentials['email'] = $username :
-            $credentials['phone'] = $username;
-
-        $credentials['password'] = $request->password;
-
-        if (!$token = Auth::guard('api')->attempt($credentials)) {
-            return $this->response->errorUnauthorized('用户名或密码错误');
+        try {
+            return $server->respondToAccessTokenRequest($serverRequest, new Psr7Response)->withStatus(201);
+        } catch(OAuthServerException $e) {
+            return $this->response->errorUnauthorized($e->getMessage());
         }
-
-        return $this->respondWithToken($token)->setStatusCode(201);
     }
 
     public function socialStore($type, SocialAuthorizationRequest $request)
@@ -79,15 +92,26 @@ class AuthorizationsController extends Controller
         return $this->respondWithToken($token)->setStatusCode(201);
     }
 
-    public function update()
+//    public function update()
+//    {
+//        $token = Auth::guard('api')->refresh();
+//        return $this->respondWithToken($token);
+//    }
+
+    public function update(AuthorizationServer $server, ServerRequestInterface $serverRequest)
     {
-        $token = Auth::guard('api')->refresh();
-        return $this->respondWithToken($token);
+        try {
+            return $server->respondToAccessTokenRequest($serverRequest, new Psr7Response);
+        } catch(OAuthServerException $e) {
+            return $this->response->errorUnauthorized($e->getMessage());
+        }
     }
 
     public function destroy()
     {
-        Auth::guard('api')->logout();
+//        Auth::guard('api')->logout();
+        // 使用 $this->user() 就能获取到令牌对应的用户模型
+        $this->user()->token()->revoke();
         return $this->response->noContent();
     }
 
